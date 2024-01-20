2397981897ccb40e6fa4c482feb50c7055805ebd
Medhat Mostafa <mmedhatmostafa3@gmail.com>
2024-01-20 12:11:25 +0200

    last update 20/1

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
-import 'package:banknote/src/app/providers/app_provider.dart';
 import 'package:banknote/src/app/providers/auth_provider.dart';
 import 'package:banknote/src/app/providers/categories_provider.dart';
 import 'package:banknote/src/app/providers/form_provider.dart';
 import 'package:banknote/src/app/providers/gift_provider.dart';
 import 'package:banknote/src/app/providers/news_provider.dart';
-import 'package:banknote/src/presentation/auth/pages/otp_page.dart';
 import 'package:banknote/src/presentation/welcome_page/splash_page.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:get/get.dart';
 import 'package:upgrader/upgrader.dart';
-
 import 'notification/my_notification.dart';
-import 'pusher/pusher_config.dart';
 import 'src/app/providers/category_provider.dart';
 import 'src/app/providers/chat_provider.dart';
 import 'src/app/providers/lang_provider.dart';
@@ -48,7 +43,7 @@ void main() async{
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ar')],
       path: 'assets/languages',
-      assetLoader: RootBundleAssetLoader(),
+      assetLoader: const RootBundleAssetLoader(),
       useOnlyLangCode: true,
       saveLocale: true,
       child: MultiProvider(
